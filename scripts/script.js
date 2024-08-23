dbc93824d6fdb8ec4998de946045fb3a9e60bca8
escott2 <etriplett14@gmail.com>
2024-08-23 11:15:51 -0500

    improve keyboard animation with setInterval

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,26 @@
-const keyE = document.getElementById("key-e");
-const keyM = document.getElementById("key-m");
-const keyI = document.getElementById("key-i");
-const keyL = document.getElementById("key-l");
-const keyY = document.getElementById("key-y");
-
 const textBox = document.querySelector(".text-box");
-let innerText = "";
+const animatedText = "Emily Scott";
+const animatedTextArray = animatedText.split("");
+let displayText = "";
+let index = 0;
+let intervalId;
+const stopTextAction = () => {
+  clearInterval(intervalId);
+}
 
 window.addEventListener("load", () => {
-  setTimeout(() => {
-    keyE.style.fill = "#B5C8AE";
-    textBox.textContent = innerText += "E";
-  }, 1000);
-  setTimeout(() => {
-    keyM.style.fill = "#B5C8AE";
-    textBox.textContent = innerText += "M";
-  }, 1500);
-  setTimeout(() => {
-    keyI.style.fill = "#B5C8AE";
-    textBox.textContent = innerText += "I";
-  }, 2000);
-  setTimeout(() => {
-    keyL.style.fill = "#B5C8AE";
-    textBox.textContent = innerText += "L";
-  }, 2500);
-  setTimeout(() => {
-    keyY.style.fill = "#B5C8AE";
-    textBox.textContent = innerText += "Y";
-  }, 3000);
+  intervalId = setInterval(() => {
+    if (index < animatedTextArray.length) {
+      const character = animatedTextArray[index];
+      displayText += character;
+      textBox.textContent = displayText;
+      let targetKey = document.getElementById(`key-${character.toLowerCase()}`);
+      if (targetKey) {
+        targetKey.style.fill = "#B5C8AE";
+      }
+      index++;
+    } else {
+      stopTextAction();
+    }
+  }, 200)
 })
